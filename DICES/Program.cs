72b7292c459f4ce6cdd78cf0f348a273add53c24
Daerato <lemmoniadowy@gmail.com>
2020-06-01 22:44:33 +0200

    Opis gry - update v.0.2.

--- DICES/Program.cs
+++ DICES/Program.cs
@@ -7,7 +7,12 @@ namespace DICES
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Kościany Poker ver.0.1 - GitHub 1st Project Pull);
+            Console.WriteLine("Kościany Poker ver.0.1 - GitHub 1st Project Pull");
+            Console.WriteLine("Poker - układ rzutów jak w klasycznym Pokerze Omaha - do 5ciu kości, rzut - blokada - rzut.");
+            Console.WriteLine("Po rundzie pierwszej wymiana, od 1 do 5 kości, runda druga rzutów - sprawdzenie układu.");
+            Console.WriteLine("Jezu, poker kościany w jaki gra się pod celą na Monte albo był w pierwszym Wiedźmaku");
+            Console.WriteLine("Przewaga dwóch zwycięstw, gra do trzech rund, porównanie z komputerem po każdej rundzie");
+            Console.ReadKey();
         }
     }
 }
