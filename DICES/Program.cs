bd472296f2f5d32ba5ff501aa64e93752567f8ed
Daerato <lemmoniadowy@gmail.com>
2020-06-30 20:18:56 +0200

    2 dice one round game need to rebuild and add 5 dices with table poker

--- DICES/Program.cs
+++ DICES/Program.cs
@@ -18,6 +18,22 @@ namespace RandomDices
             int dice1 = rnd.Next(1, 7);
             int dice2 = rnd.Next(1, 7);
 
+            Console.WriteLine("Dice 1: " + dice1);
+            Console.WriteLine("Dice 2: " + dice2);
+
+            if (dice1 > dice2)
+            {
+                Console.WriteLine("Dice 1 wins!");
+            }
+            else if (dice1 < dice2)
+            {
+                Console.WriteLine("Dice 2 wins!");
+            }
+            else
+            {
+                Console.WriteLine("Draw!");
+            }
+
 
         }
     }
