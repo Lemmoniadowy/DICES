c9bd4065bf09a73612285d22762cb528940a2542
Daerato <lemmoniadowy@gmail.com>
2020-06-30 20:03:27 +0200

    new dice project

--- DICES/Program.cs
+++ DICES/Program.cs
@@ -1,57 +1,33 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 
-namespace DICES
+namespace RandomDices
 {
     class Program
     {
         static void Opis()
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Kościany Poker ver.0.1 - GitHub 1st Project Pull");
-            Console.WriteLine("Poker - układ rzutów jak w klasycznym Pokerze Omaha - do 5ciu kości, rzut - blokada - rzut.");
-            Console.WriteLine("Po rundzie pierwszej wymiana, od 1 do 5 kości, runda druga rzutów - sprawdzenie układu.");
-            Console.WriteLine("Jezu, poker kościany w jaki gra się pod celą na Monte albo był w pierwszym Wiedźmaku");
-            Console.WriteLine("Przewaga dwóch zwycięstw, gra do trzech rund, porównanie z komputerem po każdej rundzie");
-            Console.ReadKey();
-            Console.WriteLine("Class - Dice. Każdy obiekt będzie dziedziczył po klasie głównej - tylko z innym atrybutem: 1,2,3,4,5");
-            Console.WriteLine("Class Dice -> Dice1: wynik rzutu (1,2,3,4,5,6) -> Dice2: wynik rzutu itd. itd.");
-            Console.WriteLine("Metoda Losowania rzutu dla kości - nie ma znaczenia czy user czy comp. - metoda losowania rzutu");
-            Console.WriteLine("Kto zaczyna grę? Określenie czy losowanie? Gra z drugim użytkownikiem tzw. Hot Seats?");
-            Console.WriteLine("Zapisujemy wynik do tablicy 1dno wymiarowej pięcoelementowej - kości jest 5.");
-            Console.WriteLine("Z góry określamy możliwe argumenty dla klasy Dice - 1,2,3,4,5,6 - tyle jest możliwych wyników rzutu");
-            Console.WriteLine("Z góry określamy możliwe miejsca dla wyniku - 5 - tyle ile kart w Pokerze Omaha jest na ręku.");
-            Console.ReadKey();
-            Console.WriteLine("Losowanie, dwie kostki, porównani wyniku.");
-            Console.WriteLine("Druga kostka pozostaje zakryta - blackgammon - zakład z komputerem.");
-            Console.WriteLine("Przy okazji można dodać więcej opcji, dobra kodzim na sucho na służbowej maszynce i będziemy przepisywać do domowego potato PC");
-
+            Console.WriteLine("Random Number Game");
 
         }   
-        static void Main()
+        static void Main(string []args)
         {
-
             Opis();
 
-            Console.Write("Enter the name of Player 1: ");
-            string Player1 = Console.ReadLine();
-            Console.Write("Enter the name of Player 2: ");
-            string Player2 = Console.ReadLine();
+            Random rnd = new Random();
+            int number = rnd.Next(5, 10);
+            Console.WriteLine(number);
 
-            //Random generator = new Random(0);
-            //    for (int i = 1; i < 6; i++)
-            //Console.WriteLine(generator.Next(1,7));
-            //Console.ReadKey();
+            int[] numbers = new int[10];
+            for (int i = 0; i <numbers.Length; i++ )
+            {
+                numbers[i] = rnd.Next(1, 50);
+            }
 
+            foreach ( var nr in numbers)
+            {
+                Console.Write(nr + " ");
+            }
 
-
-            var rand1 = new Random();
-            Console.WriteLine("Five random integers between 1 and 6:");
-            for (int ctr = 0; ctr <= 4; ctr++)
-                Console.Write("{0,6}", rand1.Next(1, 7));
-            Console.WriteLine();
-
-            
         }
     }
 
