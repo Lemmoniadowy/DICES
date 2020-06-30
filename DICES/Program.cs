cf50f4f0a03dc306b19d99ae2fc082d97de982e9
Daerato <lemmoniadowy@gmail.com>
2020-06-30 20:06:15 +0200

    dice 1 / dice 2

--- DICES/Program.cs
+++ DICES/Program.cs
@@ -14,19 +14,10 @@ namespace RandomDices
             Opis();
 
             Random rnd = new Random();
-            int number = rnd.Next(5, 10);
-            Console.WriteLine(number);
 
-            int[] numbers = new int[10];
-            for (int i = 0; i <numbers.Length; i++ )
-            {
-                numbers[i] = rnd.Next(1, 50);
-            }
+            int dice1 = rnd.Next(1, 7);
+            int dice2 = rnd.Next(1, 7);
 
-            foreach ( var nr in numbers)
-            {
-                Console.Write(nr + " ");
-            }
 
         }
     }
