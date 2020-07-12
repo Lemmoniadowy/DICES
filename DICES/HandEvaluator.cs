c0a5f8d4d1e79076c2266c62ec2fe522c7b05944
Daerato <lemmoniadowy@gmail.com>
2020-07-12 14:50:23 +0200

    dice 0 value 0 potem sort i wbicie do konstruktora wartości

--- DICES/HandEvaluator.cs
+++ DICES/HandEvaluator.cs
@@ -21,8 +21,51 @@ namespace DICES
         public int HighDice { get; set; }
     }
 
-    class HandEvaluator
+    class HandEvaluator : Dice
     {
+        private int oneSum;
+        private int twoSum;
+        private int threeSum;
+        private int fourSum;
+        private int fiveSum;
+        private int sixSum;
+        private int sevenSum;
+        private int eightSum;
+        private int nineSum;
+        private int tenSum;
+        private Dice[] dices;
+        private HandValue handValue;
 
+        public HandEvaluator (Dice[] sortedHand)
+        {
+            oneSum = 0;
+            twoSum = 0;
+            threeSum = 0;
+            fourSum = 0;
+            fiveSum = 0;
+            sixSum = 0;
+            sevenSum = 0;
+            eightSum = 0;
+            nineSum = 0;
+            tenSum = 0;
+            dices = new Dice[5];
+            handValue = new HandValue();
+        }
+
+        public HandValue HandValues
+        {
+            get { return handValue; }
+            set { handValue = value; }
+        }
+
+        public Dice [] Dices
+        {
+            get { return dices; }
+            set
+            {
+                dices[0] = value[0];
+            }
+
+        }
     }
 }
